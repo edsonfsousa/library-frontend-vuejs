084f9e21c1f907b64f4932920d12c0c338e96565
Edson Fernandes <65977099+edsonfsousa@users.noreply.github.com>
2021-05-16 17:21:47 -0300

    Alterado o service

--- src/services/config.js
+++ src/services/config.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const http = axios.create({
     // baseURL: 'http://localhost:8080/'
-    baseURL: 'https://api-livraria.herokuapp.com/'
-    // baseURL: 'https://springboot-api-livraria.herokuapp.com/'
+    // baseURL: 'https://api-livraria.herokuapp.com/'
+    baseURL: 'https://springboot-api-livraria.herokuapp.com/'
 });
